81a5536be3021ad00bfb3e2e3d26c921689a45f0
Wolf <caffeinate@msn.com>
2023-06-02 12:31:46 -0400

    Improved tests

--- src/bubbler.py
+++ src/bubbler.py
@@ -1,3 +1,5 @@
+import logging
+
 import matplotlib.pyplot as plt
 
 from university import Universities
@@ -9,18 +11,18 @@ def two_dimensions_test():
     universities = Universities.from_file("university/dataset.csv")
 
     clustered, centroids = universities.cluster(
-        4, ("graduates", "endowment"), resolution=120
+        4, ("overallCost", "endowment"), resolution=14
     )
     print(clustered["cluster"])
 
     plt.scatter(
-        x=clustered["graduates"],
+        x=clustered["overallCost"],
         y=clustered["endowment"],
         c=clustered["cluster"],
     )
     plt.scatter(
-        x=centroids["graduates"],
-        y=centroids["endowment"],
+        x=centroids[:, 0],
+        y=centroids[:, 1],
         c="red",
     )
     plt.show()
@@ -30,7 +32,7 @@ def three_dimensions_test():
     universities = Universities.from_file("university/dataset.csv")
 
     clustered, centroids = universities.cluster(
-        3, ("cost", "endowment", "facultySalary"), resolution=3
+        3, ("medianIncome", "endowment", "facultySalary"), resolution=50
     )
     for cluster in clustered.groupby("cluster"):
         print(cluster)
@@ -40,7 +42,7 @@ def three_dimensions_test():
     ax = fig.add_subplot(111, projection="3d")
     colors = clustered["cluster"].values
     ax.scatter(
-        xs=clustered["cost"],
+        xs=clustered["medianIncome"],
         ys=clustered["endowment"],
         zs=clustered["facultySalary"],
         c=colors,
@@ -51,7 +53,7 @@ def three_dimensions_test():
         zs=centroids[:, 2],
         c="red",
     )
-    ax.set_xlabel("Cost")
+    ax.set_xlabel("Median Income")
     ax.set_ylabel("Endowment")
     ax.set_zlabel("Faculty Salary")
     ax.set_title("Clustered Universities")
@@ -62,8 +64,9 @@ def omnidimention_test():
     universities = Universities.from_file("university/dataset.csv")
     with open("bubble_by.txt") as f:
         bubble_by = tuple(f.read().split("\n"))
+    logging.debug("Bubbling by: %s", bubble_by)
 
-    clustered, centroids = universities.cluster(5, bubble_by, resolution=3)
+    clustered, centroids = universities.cluster(4, bubble_by, resolution=8)
 
     for cluster in clustered.groupby("cluster"):
         print(cluster)
