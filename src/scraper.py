ed0e13c662fbed4d803eafadbe9b05cf17907a58
Wolf <caffeinate@msn.com>
2023-06-06 11:28:59 -0400

    Merge datasets

--- src/scraper.py
+++ src/scraper.py
@@ -1,14 +1,19 @@
 import logging
 
+import pandas as pd
+
 from university import Universities
 
 logging.basicConfig(level=logging.DEBUG)
 
 
 def main():
+    existing = Universities.from_file("university/dataset.csv")
     universities = Universities.from_schools("test.txt")
+    universities.universities = pd.concat(
+        (existing.universities, universities.universities)
+    )
     universities.to_file("university/dataset.csv")
-    print(universities)
 
 
 if __name__ == "__main__":
